0c8bd41b5d13b1020cb0d4c8f2eaec7b0eee3d9b
rim4j <amirhosseinjalali818@gmail.com>
2025-10-02 12:40:08 +0330

    create pages

---
+++ app/checkout/page.tsx
@@ -0,0 +1,5 @@
+const CheckoutPage = () => {
+  return <h1 className='text-3xl'>CheckoutPage</h1>;
+};
+
+export default CheckoutPage;
---
+++ app/favorites/page.tsx
@@ -0,0 +1,5 @@
+const FavoritesPage = () => {
+  return <h1 className='text-3xl'>FavoritesPage</h1>;
+};
+
+export default FavoritesPage;
