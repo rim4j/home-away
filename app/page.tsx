769ddcc808234a7bf223ae31a10b781c62d6f37e
rim4j <amirhosseinjalali818@gmail.com>
2025-10-02 12:29:16 +0330

    fix import globals.css in layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className='text-8xl'>hello world</h1>
+    </div>
+  );
 };
 
 export default HomePage;
