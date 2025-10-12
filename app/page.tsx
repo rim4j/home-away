815ef48990a260209d117fc8772efe3a440a3507
rim4j <amirhosseinjalali818@gmail.com>
2025-10-12 12:48:34 +0330

    install shadcnui and button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-8xl'>hello world</h1>
+      <Button variant='outline' size='lg'>
+        Click me
+      </Button>
     </div>
   );
 };
