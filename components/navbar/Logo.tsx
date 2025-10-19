edddf8ef2964954be7a28a98984378ab79955244
rim4j <amirhosseinjalali818@gmail.com>
2025-10-19 12:26:28 +0330

    logo and navsearch component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,15 @@
+import { LuTent } from "react-icons/lu";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button asChild>
+      <Link href='/'>
+        <LuTent className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
